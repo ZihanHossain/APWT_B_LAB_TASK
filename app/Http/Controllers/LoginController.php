ae3d14bccd0cb9342d241c202641929ed00006ce
zihan <zihanhossain02@gmail.com>
2021-06-23 12:49:07 +0600

    Login-Module compleated

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    function index()
+    {
+        return view('login');
+    }
+
+    function varify(Request $req)
+    {
+        $validated = $req->validate([
+            'email' => 'required|max:255|email',
+            'password' => 'required|string|min:8|max:20',
+        ]);
+
+        //$user = '';
+
+        $user = User::where('email', $req->email)
+            ->where('password', $req->password)
+            ->first();
+        
+            if(isset($user)){
+                $req->session()->put('user_name', $user->user_name);
+                return redirect('/home');
+            }else{
+                $req->session()->flash('msg', 'invaild username or password');
+                return redirect('/login');
+            }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', 'App\Http\Controllers\LoginController@index');
+Route::post('/login', 'App\Http\Controllers\LoginController@varify');
